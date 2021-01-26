7f96a891dac59d37f56686e5b783939f8f2b1f22
RossieK <rosi.kirova96@gmail.com>
2021-01-26 20:28:38 +0200

    Add handlebars and main route

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const express = require('express');
 const config = require('./config/config');
+const express = require('express');
+const handlebars = require('express-handlebars');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello.');
-})
+// Handlebars
+app.engine('hbs', handlebars({
+    extname: 'hbs'
+}));
 
+app.set('view engine', 'hbs');
+
+// Routes
+app.get('/', (req, res) => {
+    res.render('index', { layout: false });
+});
+
+// Server listening
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
