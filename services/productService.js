7d2ea8c387d58ee1cead406f9f6b49dda1b261c0
RossieK <rosi.kirova96@gmail.com>
2021-01-26 23:21:53 +0200

    Add db and working add functionality

--- services/productService.js
+++ services/productService.js
@@ -1,8 +1,20 @@
 const uniqid = require('uniqid');
 const Cube = require('../models/Cube');
+const fs = require('fs');
+const productsData = require('../config/products.json');
 
 function createProduct(data) {
     let cube = new Cube(uniqid(), data.name, data.description, data.imageUrl, data.difficultyLevel);
+
+    productsData.push(cube);
+
+    fs.writeFile(__dirname + '/../config/products.json', JSON.stringify(productsData), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+
+    });
 }
 
 module.exports = {
