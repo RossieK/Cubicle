242991adeff8d0e0f9a48ec18dfd34c43e6a6c8f
RossieK <rosi.kirova96@gmail.com>
2021-01-29 22:21:43 +0200

    Save accessory to DB

--- controllers/accessoryController.js
+++ controllers/accessoryController.js
@@ -1,9 +1,17 @@
 const { Router } = require('express');
+const accessoryService = require('../services/accessoryService');
 
 const router = Router();
 
 router.get('/create', (req, res) => {
     res.render('createAccessory');
+});
+
+//TODO: Create validation middleware or just validate incoming data
+router.post('/create', (req, res) => {
+    accessoryService.createAccessory(req.body)
+        .then(() => res.redirect('/'))
+        .catch(() => res.status(500).end());
 });
 
 module.exports = router;
