ef0ab726764430c96612ea53a5619021323bb560
RossieK <rosi.kirova96@gmail.com>
2021-01-29 22:07:43 +0200

    Add new views and accessory controller

--- routes.js
+++ routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const productController = require('./controllers/productController');
 const aboutController = require('./controllers/aboutController');
+const accessoryController = require('./controllers/accessoryController');
 
 const router = Router();
 
 router.use('/', productController);
+router.use('/accessories', accessoryController);
 router.use('/about', aboutController);
 router.get('*', (req, res) => {
     res.render('404');
