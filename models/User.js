6f4618df03ad62c4f728407e8b0bf45761c09855
RossieK <rosi.kirova96@gmail.com>
2021-02-10 21:28:45 +0200

    Add validation to User model + error notifications

--- models/User.js
+++ models/User.js
@@ -1,13 +1,33 @@
 const mongoose = require('mongoose');
+const ENGLISH_ALPHANUMERIC_PATTERN = /^[a-zA-Z0-9]+$/;
 
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        minlength: 5,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => {
+                return `Username ${props.value} is invalid, it should consist only of english letters and digits`;
+            }
+        }
     },
     password: {
         type: String,
-        required: true
+        required: true,
+        validate: {
+            validator: (value) => {
+                return ENGLISH_ALPHANUMERIC_PATTERN.test(value);
+            },
+            message: (props) => {
+                return `Invalid password, it should consist only of english letters and digits`;
+            }
+        },
+        minlength: 8
     }
 });
 
